6645bf8d3d2bb0fd8f98d12d853ab20e5ac6b9df
srushtikhatale45-sketch <srushtikhatale45@gmail.com>
2026-08-11 22:12:32 +0530

    JSX File.

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ const heading = React.createElement("h1",{id:"heading"}, "Namaste React");
 
 
 
-const jsxHeading = <h1>Namaste React using JSX!</h1>
+const jsxHeading = <h1>Namaste React using JSX !</h1>
 
 const root=ReactDOM.createRoot(document.getElementById("root"));
 console.log(heading);
